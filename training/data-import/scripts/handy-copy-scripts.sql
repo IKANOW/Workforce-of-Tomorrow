50e45605d6a5c689dbc9a1f62c6f850fac2e63c5
Chris Morgan <cmorgan@ikanow.com>
2012-04-17 14:40:04 -0400

    Update to add copy scripts to data import library

---
+++ training/data-import/scripts/handy-copy-scripts.sql
@@ -0,0 +1,4 @@
+copy tbl_workforce from '/data/workforce-4-17-2012.csv' using delimiters ',';
+copy tbl_fte_multipliers from '/data/workload-fte-multipliers-4-17-2012.csv' using delimiters ',';
+copy tbl_potential_work from '/data/workload-potential-work-4-17-2012.csv' using delimiters ',';
+copy tbl_selection_rates from '/data/workload-selection-rates-4-17-2012.csv' using delimiters ','
